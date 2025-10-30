5408b181a7d93a8b5b84471e7995a6eba1fb63f2
Sidneygomes878 <sidneygomesdev@gmail.com>
2025-10-30 16:39:35 -0300

    Apagando arquivos

--- SegundoModulo/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-public static void main(String[] args) {
-    
-    String nomeDoAluno = "Sidney Gomes"; 
-    
-    double media = (int) (6.8 + 7.9 + 8.2) / 3;
-    System.out.println("ola " + nomeDoAluno + " a sua media e " + media);
-}
-}
